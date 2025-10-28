b3731efa48a098a8a964dca435d756dfcce65d8a
Stiliyanss <stilianss03@gmail.com>
2025-10-28 13:49:42 +0200

    Replace 'C' with trash image

--- src/components/Keys.jsx
+++ src/components/Keys.jsx
@@ -1,14 +1,25 @@
+import trashIcon from '../assets/trash.png'
+
 export default function Keys({ label, className, onButtonClick }) {
   return (
     <button
       className={`
         bg-gray-600 text-white rounded-xl py-4 text-xl font-medium
         hover:bg-gray-500 active:bg-gray-400 transition-colors
+        flex items-center justify-center
         ${className || ""}
       `}
       onClick={()=>onButtonClick(label)}
     >
-      {label}
+       {label === "C" ? (
+        <img
+          src={trashIcon}
+          alt="delete"
+          className="w-6 h-6"
+        />
+      ) : (
+        label
+      )}
     </button>
   );
 }
